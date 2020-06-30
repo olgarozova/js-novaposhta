511ed0df6ee6dbadad226abf9ddbfd804612bac3
Olga Rozova <olya.smolina@gmail.com>
2020-06-30 19:10:14 +0300

    add/remove history, view history block

--- main.js
+++ main.js
@@ -1,12 +1,60 @@
+
+/*class RequestFactory {
+
+     createRequest (type,apiKey,ttnId = '') {     
+        let body; 
+        if (type === 'Treking') {
+            body = new Treking(ttnId);
+        }else if(type === 'TtnList'){
+            body = new TtnList();
+        }
+        body.apiKey = apiKey;
+
+    }
+  }
+
+class Treking {
+    constructor(ttnId){               
+        this.modelName = "TrackingDocument";
+        this.calledMethod = "getStatusDocuments";
+        this.methodProperties = {
+            "Documents": [
+                {
+                    "DocumentNumber": ttnId,
+                    "Phone":""
+                },                    
+            ]
+        };
+        
+    }
+}
+class TtnList {
+    constructor(DateFrom,DateTo){               
+        this.modelName = "InternetDocument";
+        this.calledMethod = "getDocumentList";
+        this.methodProperties = {
+            "DateTimeFrom" : DateFrom,
+            "DateTimeFrom" : DateTo,
+            "Page" :1,
+            "GetFullList": "0"
+        };
+    }
+        
+}*/
+/***********************************************************/
+
+
 class TTNApi {
     constructor(apiKey){
-        this.apiKey = apiKey;           
+        this.apiKey = apiKey;        
     }
 
     getTTN(ttnId){
         console.log('send request for status of ' + ttnId);
+       // let body = RequestFactory.createRequest('Treking',apiKey,ttnId);
+
         
-        let body = {
+        let body = { //TODO : create new class Treking
             "apiKey": this.apiKey,
             "modelName": "TrackingDocument",
             "calledMethod": "getStatusDocuments",
@@ -21,7 +69,7 @@ class TTNApi {
             
         };
 
-        let response = fetch('https://api.novaposhta.ua/v2.0/json/',{
+       const response = fetch('https://api.novaposhta.ua/v2.0/json/',{
             method: 'POST',
             body: JSON.stringify(body)
         })
@@ -30,20 +78,14 @@ class TTNApi {
         .then(data =>  { // create TTN obj 
             const ttn = new TTN(data.Number,
                                 data.StatusCode,
-                                data.Status);   //add  WarehouseRecipient, WarehouseSender 
-            ttn.viewStatusInfo();
-            ttn.addToHistory();   
+                                data.Status);   //TODO: add  WarehouseRecipient, WarehouseSender 
           
             return ttn;    
         })       
         .catch(error => alert('HTTP request error.  ' + error)); 
-       
-                      
-    }
-    
-    viewHistoryBlock(){
-        console.log('render html history block');
-    }
+
+        return response;                    
+    }       
     
     //my individual function
 }
@@ -55,23 +97,34 @@ class TTN {
         this.status = status;
     }
     viewStatusInfo(){
-        //return HTML str
+        //return HTML 
         console.log('render html status info');
     }
     addToHistory(){
         //save with localStorage ?
         console.log('add to history '+this.ttnId);
+        
+        let ttns = JSON.parse(localStorage.getItem('historyTtns') || '[]' );
+       
+        if(!ttns.includes(this.ttnId)) {
+            ttns.push(this.ttnId);        
+            localStorage.setItem('historyTtns',JSON.stringify(ttns)); 
+            TTN_History.viewHistoryBlock();               
+        }
     }
 }
 
 class TTNForm {
+
     constructor(apiKey){
         this.form = document.querySelector("#ttn-status-form"); 
-        this.ttnNumberElem = this.form.querySelector("#ttn_number");
+        this.ttnNumberElem = this.form.querySelector("#ttn_number");        
            
         this.regExForTTN = /^(5|2|1)[0-9]{13}$/; // common regEx : firstsymbol 5|2|1 , 14 symbols in ttn, only numeric
         this.TTNApi = new TTNApi(apiKey);
+        
     }
+
     isValidNumber(){        
         if (!this.regExForTTN.test(this.ttnNumberElem.value)) {
             console.log(this.ttnNumberElem.value + ' is not valid');
@@ -83,13 +136,79 @@ class TTNForm {
         event.preventDefault();              
 
         if(this.isValidNumber()){
-            this.TTNApi.getTTN(this.ttnNumberElem.value);                    
+            const response = this.TTNApi.getTTN(this.ttnNumberElem.value);                           
+
+            //add to history here!               
+            response.then(ttn => {
+                ttn.viewStatusInfo();
+                ttn.addToHistory();
+            });
+            
         }      
-    }
+    }    
+
     init(){        
-        this.form.onsubmit = this.getInfo.bind(this);
-        this.TTNApi.viewHistoryBlock();
+        this.form.onsubmit = this.getInfo.bind(this);       
+        TTN_History.viewHistoryBlock();
     }    
 }
+/***************************************************/
+
+class TTN_History{
+    static getHistoryTtns(){
+        let ttns = JSON.parse(localStorage.getItem('historyTtns') || '[]');       
+        return ttns;
+    }
+    static getHistoryInfo(event){ 
+        // event.preventDefault();              
+ 
+         if(this.isValidNumber()){
+             this.TTNApi.getTTN(this.ttnNumberElem.value);     
+             //add to history here!               
+         }      
+     }
+
+    static viewHistoryBlock(){
+        const ttnHistoryContainer = document.querySelector(".ttns-history__result");
+        const ttns = this.getHistoryTtns();
+        console.log('render html history block');
+
+        if(!!ttns.length){
+           
+            const ul = document.createElement('ul');          
+           
+            ttns.forEach(ttn => { 
+
+                const li = document.createElement('li');//add onclick event
+                                
+                li.onclick = this.getHistoryInfo;
+                ul.appendChild(li);
+                li.innerHTML = ttn;
+
+            });
+            ttnHistoryContainer.innerHTML = '';            
+            ttnHistoryContainer.appendChild(ul);
+            
+
+          
+            //add button 'clear history'
+            const btnClear = document.createElement("button");
+            btnClear.innerHTML = "Clear history";
+            btnClear.onclick = this.deleteHistory.bind(this);                
+            ttnHistoryContainer.appendChild(btnClear);  
+        }else{           
+            ttnHistoryContainer.innerHTML = 'The history is empty';
+        }
+    }
+    static deleteHistory(){
+        console.log('delete all history');
+        localStorage.removeItem('historyTtns');
+        this.viewHistoryBlock();
+    }
+
+}
+
+
+
 const novaPoshtaForm = new TTNForm('f32192aa4b7940e82fbe254e62673948');
 novaPoshtaForm.init();
