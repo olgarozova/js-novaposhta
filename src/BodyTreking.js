d02ef27bfc5e4c99aa364d5201d213a6ff4b07db
Olga Rozova <olya.smolina@gmail.com>
2020-07-23 20:47:18 +0300

    reorganize bodyCreator.js

---
+++ src/BodyTreking.js
@@ -0,0 +1,18 @@
+class BodyTreking{
+    constructor(data){
+        const {ttnId} = data;                              
+        this.modelName = "TrackingDocument";
+        this.calledMethod = "getStatusDocuments";
+        this.methodProperties = {
+            "Documents": [
+                {
+                    "DocumentNumber": ttnId,
+                    "Phone":""
+                },                    
+            ]
+        };        
+
+    }
+}
+
+export default BodyTreking;
---
+++ src/BodyWarehouses.js
@@ -0,0 +1,12 @@
+class BodyWarehouses{
+    constructor(data){
+        this.modelName = "AddressGeneral",
+        this.calledMethod = "getWarehouses",
+        this.methodProperties = {
+            "Language": "ru"        
+        }       
+
+    }
+}
+
+export default BodyWarehouses;
